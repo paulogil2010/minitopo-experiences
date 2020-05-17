37c4a085f31dc7515c1f1d672065590916a45c03
Paulo Lenz <paulogil2010@gmail.com>
2020-05-17 16:59:33 -0300

    ajustes

--- Globecom/run.sh
+++ Globecom/run.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-cd quic/LO-Result
+
+# QUIC
+cd quic/BL_1-5-Result
+start=$(date +%H:%M:%S)
+
 sudo ./quic.py
-cd ../../tcp/LO-Result
+./2-flows-quic.sh
+./if-stat.sh
+
+cd ../../
+end=$(date +%H:%M:%S)
+
+echo "Iniciou - $start\nTerminou - $end" > quic_BL_1-5-Result-time.txt
+
+# TCP
+cd tcp/BL_1-5-Result
+start=$(date +%H:%M:%S)
+
 sudo ./tcp.py
-hora=$(date +%H:%M:%S)
-echo $hora > acabou.txt
+./2-flows-tcp.sh
+./if-stat.sh
+
+cd../../
+end=$(date +%H:%M:%S)
+
+echo "Iniciou - $start\nTerminou - $end" > tcp_BL_1-5-Result-time.txt
---
+++ tests/tcp/if-stat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+raiz=$(pwd)
+pasta=$(ls -d */ | grep -v core | grep -v ifstat | grep -v ifstat)
+saida="$raiz/ifstat"
+cen=1
+aux=1
+
+if [ ! -d "$saida" ]; then
+    mkdir $saida
+else
+    rm -rf $saida/*
+fi
+
+
+for dir in $pasta
+do
+    cd $dir
+    ins=$(ls | cut -d' ' -f 9)
+
+    for sub in $ins
+    do
+        FILE=$saida/ifstat.$cen.$aux.txt
+        if [ ! -f $FILE ]; then
+            touch $FILE
+            echo "eth0-in,eth0-out,eth1-in,eth1-out" > $FILE
+        fi
+
+        cat $sub/quic/1/client_ifstat.txt | sed '/KB\/s/g' | sed '/Clie/d' | awk -v OFS="," '$1=$1' >> $FILE
+        aux=$((aux+1))
+    done
+    cen=$((cen+1))
+    aux=1
+    cd $raiz
+done
